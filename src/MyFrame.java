91708665498b64050001b9d7f15bee4a6db9392c
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-25 12:03:30 +0100

    0.02

---
+++ src/MyFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        super( "Not Hello World" );
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 100);
+        setLocation(50,50);
+
+        add(new JButton("Przycisk 1"));
+        add(new JButton("Przycisk 2"));
+        add(new JButton("Przycisk 3"));
+
+        setVisible(true);
+    }
+
+}
