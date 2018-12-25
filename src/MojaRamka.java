be6b0090e3f56f38b77a08c3c94d676fe764c724
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-25 11:56:50 +0100

    Initial commit

---
+++ src/MojaRamka.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class MojaRamka extends JFrame {
+
+    public MojaRamka() {
+        super("WItam");
+        setSize(1000,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
